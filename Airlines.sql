4fa1f86bb23ff4008c0f9500357eafd78f69f522
Anil Mahadev <anilmahadev@outlook.com>
2023-12-06 16:35:58 -0600

    Update Airlines.sql

--- Airlines.sql
+++ Airlines.sql
@@ -1,42 +1,42 @@
-Creating an Airline Database involves designing various tables to represent entities like flights, passengers, aircraft, pilots, and bookings. Below is a conceptual design for such a database with SQL DDL statements to create the tables and establish their relationships.
+--Creating an Airline Database involves designing various tables to represent entities like flights, passengers, aircraft, pilots, and bookings. Below is a conceptual design for such a database with SQL DDL statements to create the tables and establish their relationships.
 
-### Entities and Their Relationships:
+--### Entities and Their Relationships:
 
-1. **Flights**: Details about each flight.
-2. **Aircraft**: Information about aircraft.
-3. **Pilots**: Information about pilots.
-4. **Passengers**: Details of passengers.
-5. **Bookings**: Records of flight bookings by passengers.
-6. **FlightCrew**: Assigning crew to flights.
+--1. Flights: Details about each flight.
+--2. Aircraft: Information about aircraft.
+--3. Pilots: Information about pilots.
+--4. Passengers: Details of passengers.
+--5. Bookings: Records of flight bookings by passengers.
+--6. FlightCrew: Assigning crew to flights.
 
-The relationships between these entities can be defined as follows:
-- A flight is associated with one aircraft and one or more pilots.
-- Passengers make bookings for flights.
-- A flight crew is assigned to each flight.
+--The relationships between these entities can be defined as follows:
+--- A flight is associated with one aircraft and one or more pilots.
+--- Passengers make bookings for flights.
+--- A flight crew is assigned to each flight.
 
-### SQL Statements to Create Tables:
+-- SQL Statements to Create Tables:
 
-1. **Aircraft Table**:
-   ```sql
+-- Aircraft Table:
+   
    CREATE TABLE Aircraft (
        AircraftID INT PRIMARY KEY IDENTITY(1,1),
        Model NVARCHAR(50) NOT NULL,
        Capacity INT NOT NULL
    );
-   ```
+   
 
-2. **Pilots Table**:
-   ```sql
+--Pilots Table:
+   
    CREATE TABLE Pilots (
        PilotID INT PRIMARY KEY IDENTITY(1,1),
        Name NVARCHAR(100) NOT NULL,
        LicenseNumber NVARCHAR(50) NOT NULL,
        Rank NVARCHAR(50)
    );
-   ```
+   
 
-3. **Flights Table**:
-   ```sql
+-- Flights Table:
+   
    CREATE TABLE Flights (
        FlightID INT PRIMARY KEY IDENTITY(1,1),
        DepartureTime DATETIME NOT NULL,
@@ -46,20 +46,19 @@ The relationships between these entities can be defined as follows:
        AircraftID INT,
        FOREIGN KEY (AircraftID) REFERENCES Aircraft(AircraftID)
    );
-   ```
+   
 
-4. **Passengers Table**:
-   ```sql
+--Passengers Table:
+   
    CREATE TABLE Passengers (
        PassengerID INT PRIMARY KEY IDENTITY(1,1),
        FirstName NVARCHAR(50) NOT NULL,
        LastName NVARCHAR(50) NOT NULL,
        PassportNumber NVARCHAR(50) NOT NULL
    );
-   ```
-
-5. **Bookings Table**:
-   ```sql
+   
+--Bookings Table:
+   
    CREATE TABLE Bookings (
        BookingID INT PRIMARY KEY IDENTITY(1,1),
        FlightID INT NOT NULL,
@@ -69,10 +68,10 @@ The relationships between these entities can be defined as follows:
        FOREIGN KEY (FlightID) REFERENCES Flights(FlightID),
        FOREIGN KEY (PassengerID) REFERENCES Passengers(PassengerID)
    );
-   ```
+   
 
-6. **FlightCrew Table**:
-   ```sql
+--FlightCrew Table:
+   
    CREATE TABLE FlightCrew (
        FlightCrewID INT PRIMARY KEY IDENTITY(1,1),
        FlightID INT NOT NULL,
@@ -81,14 +80,4 @@ The relationships between these entities can be defined as follows:
        FOREIGN KEY (FlightID) REFERENCES Flights(FlightID),
        FOREIGN KEY (PilotID) REFERENCES Pilots(PilotID)
    );
-   ```
-
-### Additional Considerations:
-
-- **Indexes**: Consider adding indexes on frequently searched columns like FlightID in Bookings and Flights tables for faster query performance.
-- **Constraints and Validations**: Additional constraints (like CHECK constraints for valid dates, times, and seat numbers) can be added to ensure data integrity.
-- **Normalization**: The design can be further normalized if necessary to reduce redundancy.
-- **Data Types and Sizes**: Adjust the data types and sizes according to your specific data requirements.
-- **Additional Entities**: Depending on the requirements, you might also need tables for airports, flight schedules, ticket prices, etc.
-
-This schema provides a foundational structure for an airline database. You can expand or modify it based on the specific needs and business rules of the airline system you are designing.
+   
